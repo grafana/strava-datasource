27665e4bc7cdc4e23ca44b5de9e2e2ac712cd917
Alexander Zobnin <alexanderzobnin@gmail.com>
2019-11-29 12:45:36 +0300

    query editor WIP

--- src/types.ts
+++ src/types.ts
@@ -10,4 +10,29 @@ export interface StravaSecureJsonData {
 }
 
 export interface StravaQuery extends DataQuery {
+  queryType: StravaQueryType;
+  activityStat: StravaActivityStat;
+}
+
+export interface StravaQueryTypeOption {
+  value: StravaQueryType;
+  label: string;
+  description?: string;
+}
+
+export interface StravaActivityStatOption {
+  value: StravaActivityStat;
+  label: string;
+  description?: string;
+}
+
+export enum StravaQueryType {
+  Activities = 'Activities',
+}
+
+export enum StravaActivityStat {
+  Distance = 'distance',
+  MovingTime = 'moving_time',
+  ElapsedTime = 'elapsed_time',
+  ElevationGain = 'total_elevation_gain',
 }
