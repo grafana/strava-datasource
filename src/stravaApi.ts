8451b5e6f9d3ad61d947a3011e763b4a15b7f672
Alexander Zobnin <alexanderzobnin@gmail.com>
2019-11-30 21:32:54 +0300

    deduplicate api requests

--- src/stravaApi.ts
+++ src/stravaApi.ts
@@ -1,8 +1,10 @@
 export default class StravaApi {
   apiUrl: string;
+  promises: any;
 
   constructor(url: string, private backendSrv: any) {
     this.apiUrl = url;
+    this.promises = {};
   }
 
   async getAuthenticatedAthlete(params?: any) {
@@ -37,6 +39,10 @@ export default class StravaApi {
   }
 
   async request(url: string, params?: any) {
+    return this.proxyfy(this._request, '_request', this)(url, params);
+  }
+
+  async _request(url: string, params?: any) {
     try {
       const { data } = await this.backendSrv.datasourceRequest({
         url: `${this.apiUrl}/strava/${url}`,
@@ -49,4 +55,49 @@ export default class StravaApi {
       throw error;
     }
   }
+
+  proxyfy(func, funcName, funcScope) {
+    if (!this.promises[funcName]) {
+      this.promises[funcName] = {};
+    }
+    const promiseKeeper = this.promises[funcName];
+    return callOnce(func, promiseKeeper, funcScope);
+  }
 }
+
+/**
+ * Wrap request to prevent multiple calls
+ * with same params when waiting for result.
+ */
+function callOnce(func, promiseKeeper, funcScope): (...args: any[]) => any {
+  return function() {
+    var hash = getRequestHash(arguments);
+    if (!promiseKeeper[hash]) {
+      promiseKeeper[hash] = Promise.resolve(
+        func.apply(funcScope, arguments)
+        .then(result => {
+          promiseKeeper[hash] = null;
+          return result;
+        })
+      );
+    }
+    return promiseKeeper[hash];
+  };
+}
+
+function getRequestHash(args) {
+  const argsJson = JSON.stringify(args);
+  return getHash(argsJson);
+}
+
+function getHash(srt: string) {
+  var hash = 0, i, chr, len;
+  if (srt.length !== 0) {
+    for (i = 0, len = srt.length; i < len; i++) {
+      chr   = srt.charCodeAt(i);
+      hash  = ((hash << 5) - hash) + chr;
+      hash |= 0; // Convert to 32bit integer
+    }
+  }
+  return hash;
+};
