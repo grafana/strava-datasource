d666f6276c4c692714f606a70dcb7b07657261d2
Peter Holmberg <peter.hlmbrg@gmail.com>
2020-05-12 09:13:44 +0200

    remove typings files

--- src/components/AthleteLabel.d.ts
+++
@@ -1,6 +0,0 @@
-import { FC } from 'react';
-interface Props {
-    athlete: any;
-}
-export declare const AthleteLabel: FC<Props>;
-export {};
--- src/module.d.ts
+++
@@ -1,4 +0,0 @@
-import { DataSourcePlugin } from '@grafana/data';
-import StravaDatasource from './datasource';
-import { StravaJsonData, StravaQuery } from './types';
-export declare const plugin: DataSourcePlugin<StravaDatasource, StravaQuery, StravaJsonData>;
